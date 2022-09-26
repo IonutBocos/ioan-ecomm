befcbed641fc957696a65eb7641bce8452c3c6bb
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-09-26 21:44:36 +0300

    944

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main className="font-bold underline bg-red-500">homepage</main>
+    </>
+  );
+};
+
+export default Home;
