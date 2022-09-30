9d0180599ca56abc2bd269f4e5f970087ebfee37
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-09-30 08:09:18 +0300

    toogle menu

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,37 +1,50 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { CgMenu } from 'react-icons/cg';
-import { FaApple } from 'react-icons/fa';
+import { FaGlasses } from 'react-icons/fa';
+import { CgClose } from 'react-icons/cg';
 
 export const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const hamburgerIcon = (
+    <CgMenu
+      size="32"
+      onClick={() => {
+        setMenuOpen(!menuOpen);
+      }}
+    ></CgMenu>
+  );
+  const closeIcon = (
+    <CgClose
+      size="32"
+      onClick={() => {
+        setMenuOpen(!menuOpen);
+      }}
+    ></CgClose>
+  );
+  if (menuOpen === true) {
+  }
 
   return (
     <>
       <section className="flex justify-between items-center h-full z-10 relative">
         <Link href="/">
           <a title="Home">
-            <FaApple size="32"></FaApple>
+            <FaGlasses size="32"></FaGlasses>
           </a>
         </Link>
 
-        <button
-          title="Menu"
-          type="button"
-          onClick={() => {
-            setMenuOpen(!menuOpen);
-          }}
-        >
-          <CgMenu size="32"></CgMenu>
+        <button title="Menu" type="button">
+          {menuOpen ? closeIcon : hamburgerIcon}
         </button>
       </section>
 
       <nav
         className={`absolute left-0 ${
           menuOpen ? 'top-0' : '-top-full'
-        } h-screen w-screen text-white bg-neutral-900`}
+        } h-screen w-screen text-white bg-neutral-900 flex justify-center  items-center `}
       >
-        <ul>
+        <ul className="flex flex-col gap-9">
           <li>
             <Link href="/">
               <a title="Home">Home</a>
