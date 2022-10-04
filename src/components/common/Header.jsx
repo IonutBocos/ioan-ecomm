3168e793f998932da6bf8151e9c2635bd53c405a
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-10-04 09:28:52 +0300

    header and layout

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import { CgMenu } from 'react-icons/cg';
 import { FaGlasses } from 'react-icons/fa';
 import { CgClose } from 'react-icons/cg';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiTwitter } from 'react-icons/fi';
+import { TiSocialGooglePlus } from 'react-icons/ti';
 
 export const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -27,7 +30,7 @@ export const Header = () => {
 
   return (
     <>
-      <section className="flex justify-between items-center h-full z-10 relative">
+      <section className="flex justify-between items-center lg:flex-col h-full z-10 relative lg:py-16 ">
         <Link href="/">
           <a title="Home">
             <FaGlasses size="32"></FaGlasses>
@@ -37,17 +40,49 @@ export const Header = () => {
         <button title="Menu" type="button">
           {menuOpen ? closeIcon : hamburgerIcon}
         </button>
+
+        <div className="hidden lg:block">
+          <ul className="flex-row gap-5">
+            <li>
+              <Link href="/">
+                <a title="Home">
+                  <FaFacebookF size="22"></FaFacebookF>
+                </a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/">
+                <a title="Home">
+                  <FiTwitter size="22"></FiTwitter>
+                </a>
+              </Link>
+            </li>
+
+            <Link href="/">
+              <a title="Home">
+                <TiSocialGooglePlus size="22px"></TiSocialGooglePlus>
+              </a>
+            </Link>
+          </ul>
+        </div>
       </section>
 
       <nav
-        className={`absolute left-0 ${
-          menuOpen ? 'top-0' : '-top-full'
-        } h-screen w-screen text-white bg-neutral-900 flex justify-center  items-center `}
+        className={`absolute left-0 -top-full transition-transform transform-gpu  ${
+          menuOpen ? 'translate-y-full' : ''
+        } h-screen w-screen lg:w-screen-1/3 text-white bg-neutral-900 flex justify-center  items-center `}
       >
-        <ul className="flex flex-col gap-9">
+        <ul className="flex flex-col gap-9 uppercase">
           <li>
             <Link href="/">
               <a title="Home">Home</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/contact">
+              <a title="Contact">About</a>
             </Link>
           </li>
 
